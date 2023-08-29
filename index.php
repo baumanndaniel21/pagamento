fd10ee2418804d197ee98c3b828b63c27290c200
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2023-08-29 08:48:15 -0300

    update

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt_br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Folha de pagamento</title>
 </head>
+
 <body>
     <h1>Folha de pagamento</h1>
-   <form>
+    <form>
         <label>Quanto você ganha por hora?</label>
         <input type="text" name="vl_hora" required\>
-   </form> 
+        <br/>
+        <label>Quanto tempo você trabalhou?</label>
+        <input type="number"
+        
+
+    </form>
 </body>
+
 </html>
