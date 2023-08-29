34f3ea79da11360c6889a2dbc5b37ca34116cbee
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2023-08-29 10:14:50 -0300

    validação de formulário

--- index.php
+++ index.php
@@ -8,19 +8,26 @@
 </head>
 
 <body>
-    <h1>--Folha de pagamento--</h1>
+    <h1>Folha de pagamento</h1>
     <form method="post">
         <label>Quanto você ganha por hora?</label>
-        <input type="number" name="vl_hora" placeholder="R$" required\>
+        <input type="number" name="vl_hora" placeholder="R$" required \>
         <br />
         <label>Quantas horas você trabalhou?</label>
-        <input type="number" name="hora_trabalhada" required placeholder="hora" />:
-        <input type="number" name="minuto_trabalhado" required placeholder="minutos" />
+        <input type="number" name="hora_trabalhada" required placeholder="hora" maxlength="2"/>:
+        <input type="number" name="minuto_trabalhado" required placeholder="minutos"  min="0" max="59" maxlength="2"/>
         <br />
         <input type="submit" value="Calcular" />
     </form>
     <?php 
-    
+    if($_POST){
+        //Atribuir os valores nas variáveis recebidas do formulário
+        $valorHora = $_POST['vl_hora'];
+        $horaTrabalhada = $_POST['hora_trabalhada'];
+        $minutoTrabalhado = $_POST['minuto_trabalhado'];
+        echo ("Valor da hora R$ $valorHora");
+
+    }//Fim IF Post
     ?>
 </body>
 
