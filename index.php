c996143ff453fedc75e4f912a49fd0d018362f7e
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2023-08-29 08:29:24 -0300

    criação do index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Folha de pagamento</title>
+</head>
+<body>
+    <h1>Folha de pagamento</h1>
+   <form>
+        <label>Quanto você ganha por hora?</label>
+        <input type="text" name="vl_hora" required\>
+   </form> 
+</body>
+</html>
