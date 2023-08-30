eddc1f9fa3fdbbc50d31290bd48709847853adbd
Daniel Baumann <143436969+baumanndaniel21@users.noreply.github.com>
2023-08-30 11:07:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -46,6 +46,15 @@
         }elseif($salarioTemp >= 1903.99 && $salarioTemp <=2826.65){
             return round(0.075*$salarioTemp-142.80,2);
         }
+        elseif($salarioTemp >=  2826.6 && $salarioTemp <= 3751.05){
+            return round(0.15*$salarioTemp-354.80,2);
+        }
+        elseif($salarioTemp >=   3751.06  && $salarioTemp <= 4664.68){
+            return round(0.225*$salarioTemp-636,13,2);
+        }
+        else{
+            return round(0.275*$salarioTemp-869.36,2);
+        }
     }//Fim Função
     if ($_POST) {
 
